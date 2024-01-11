0cae09daa9755fb6dbf74d58a1317b0b42820bbe
blackamber1978 <150267998+bentiger1904@users.noreply.github.com>
2024-01-11 21:16:19 +0000

    questions.js file completed

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "Q1. Commonly used data types do not include?",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts",
+    },
+    {
+      title: "Q2. The condition in an if/else statement is enclosed with ____________?",
+      choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+      answer: "Parenthesis",
+    },
+    {
+      title: "Q3. Arrays in Javascript can be used to store?",
+      choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All Of The Above"],
+      answer: "All Of The Above",
+    },
+    {
+      title: "Q4. String values must be contained within _________ when being assigned to variables?",
+      choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+      answer: "Quotes",
+    },
+    {
+      title: "Q5. A very useful tool during development and debugging for printing content to the debugger is?",
+      choices: ["Javascript", "Terminal", "For Loops", "Console Logs"],
+      answer: "Javascript",
+    },
+  ];
